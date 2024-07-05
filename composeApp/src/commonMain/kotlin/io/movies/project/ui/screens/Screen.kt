c530501193abbfc3846ab99b7997f6661f5de877
Guillermo Ramirez <ramirezjose@umes.edu.gt>
2024-07-05 17:18:52 -0600

    Se refactorizo el codigo: - Se creo un archivo de recuros para las cadenas de texto - Se creo un HomeScreen para pasar parte del codigo ahi

---
+++ composeApp/src/commonMain/kotlin/io/movies/project/ui/screens/Screen.kt
@@ -0,0 +1,18 @@
+package io.movies.project.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MovableContent
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Screen(modifier: Modifier = Modifier, content: @Composable () -> Unit){
+    MaterialTheme{
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            content = content
+        )
+    }
+}
